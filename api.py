744852c728fc2c03ac1a3ccebbee178c879a001a
evanwb <evanwburton@gmail.com>
2021-10-22 23:35:16 -0400

    updated html and css

--- api.py
+++ api.py
@@ -3,30 +3,32 @@ from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
-code = input('Enter your state\r\n').lower()
+global api_key
 api_key='pQNtyh3wP65oZT5zeLWTMAIRTHTEnTCQE1nRHz9L'
 
-url = 'https://developer.nps.gov/api/v1/parks?stateCode={}&api_key={}'.format(code,api_key)
-response = requests.get(url, verify=False)
+
+
 
 def print_json(input_obj):
     res = json.dumps(input_obj, sort_keys=True, indent=5)
     print(res)
     
-js = response.json()
 
-info = json.dumps(js)
+def park_total(code):
+    url = 'https://developer.nps.gov/api/v1/parks?stateCode={}&api_key={}'.format(code,api_key)
+    response = requests.get(url, verify=False)
+    js = response.json()
+    if len(js["data"]) == 0:
+        res = 'Please enter a valid state'
+    else:     
+        res = 'There are a total of {} parks in {}'.format(len(js["data"]), code.upper())
+    return (res)
 
-#print(js.items())
 
-total = 'There are a total of {} parks in {}'.format(len(js["data"]), code.upper())
-print(total)
-
-
-for i in range(len(js["data"])):
+""" for i in range(len(js["data"])):
 	list = [js["data"][i]["fullName"], js["data"][i]["parkCode"]] 
 	text = '{} Park name: {}, Park Code: {}'.format(i+1,list[0],list[1])
-	print(text+'\r\n')
+	print(text+'\r\n') """
 #print_json(js["data"][0]["fullName"])
 
 #print_json(response.json())
--- app.py
+++ app.py
@@ -1,18 +1,21 @@
 from flask import Flask, Response, render_template, request
-from search import read_input
+import api
 
+from threading import Thread  
 app = Flask(__name__)
 @app.route('/')
 
 def home():
     return render_template('home.html')
 
-@app.route('/parse_data', methods=['GET', 'POST'])
+@app.route('/data', methods=['GET', 'POST'])
 def user_input():
-    text = request.form['text']
-    processed_text = text.upper()
-    return (processed_text)
+    text = request.form['text'].lower()
+    body = api.park_total(text)
+    
+    return render_template('search.html', term=text.upper(), body=body)
+
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=3030, threaded=True)
+    app.run(host='0.0.0.0', port=80, threaded=True)
