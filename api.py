c360bc6c1617ad60c0b7f86c1edd72051b163754
evanwb <evanwburton@gmail.com>
2021-10-24 23:35:50 -0400

    park search and pages

--- api.py
+++ api.py
@@ -1,34 +1,84 @@
 import requests, json
+from util import *
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 global api_key
+global parks
+
 api_key='pQNtyh3wP65oZT5zeLWTMAIRTHTEnTCQE1nRHz9L'
 
+def park_search(park_code):
+    url = 'https://developer.nps.gov/api/v1/parks?parkCode={}&api_key={}'.format(park_code,api_key)
+    response = requests.get(url, verify=False)
+    data = response.json()
+
+    for i in range(len(data["data"])):
+        park = {
+            "name": data["data"][0]["fullName"],
+            "code": data["data"][0]["parkCode"],
+            "desc": data["data"][0]["description"],
+            "act": data["data"][0]["activities"]
+        }
+    return park
 
 
 
-def print_json(input_obj):
-    res = json.dumps(input_obj, sort_keys=True, indent=5)
-    print(res)
+
+def state_search(state_code):
+    url = 'https://developer.nps.gov/api/v1/parks?stateCode={}&api_key={}'.format(state_code,api_key)
+    response = requests.get(url, verify=False)
+    data = response.json()
+
+    park_names = []
+    park_codes = []
+    park_descs = []
+    park_acts = []
+    parks = []
+  
+    for i in range(len(data["data"])):
+        park = {
+            "name": data["data"][i]["fullName"],
+            "code": data["data"][i]["parkCode"],
+            "desc": data["data"][i]["description"],
+            "act": data["data"][i]["activities"]
+        }
+
+        #names = names.split("\r\n")
+        '''park_names.append(name)
+        park_codes.append(code)
+        park_descs.append(desc)'''
+        parks.append(park)
+        #text = '{} \r\n'.format(names)
     
+    
+    return parks
+    #return list(zip(park_names,park_codes,park_descs,park_acts))
+
 
 def park_total(code):
     url = 'https://developer.nps.gov/api/v1/parks?stateCode={}&api_key={}'.format(code,api_key)
     response = requests.get(url, verify=False)
-    js = response.json()
-    if len(js["data"]) == 0:
+    data = response.json()
+    if len(data["data"]) == 0:
         res = 'Please enter a valid state'
     else:     
-        res = 'There are a total of {} parks in {}'.format(len(js["data"]), code.upper())
+        res = 'There are a total of {} parks in {}'.format(len(data["data"]), get_state_name(code))
+
     return (res)
 
+def cli_test(code):
+    parks = park_search(code)
+    print(parks)
 
-""" for i in range(len(js["data"])):
-	list = [js["data"][i]["fullName"], js["data"][i]["parkCode"]] 
-	text = '{} Park name: {}, Park Code: {}'.format(i+1,list[0],list[1])
-	print(text+'\r\n') """
-#print_json(js["data"][0]["fullName"])
+    '''for park in parks:
+        print('{}\r'.format(park['name']))
+        print('{}\r'.format(park['code']))
+        print('{}\r'.format(park['desc']))
+        acts = park['act']
+        for act in acts:
+            print(act['name'])'''
+    print('\r\n')
 
-#print_json(response.json())
+#cli_test(input('code'))
--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask, Response, render_template, request
-import api
+from flask import Flask, Response, render_template, request, flash, get_flashed_messages
+from flask_session.__init__ import Session
+from threading import Thread 
+from api import *
+from util import *
+import os
 
-from threading import Thread  
+ 
 app = Flask(__name__)
 @app.route('/')
 
@@ -10,12 +14,33 @@ def home():
 
 @app.route('/data', methods=['GET', 'POST'])
 def user_input():
-    text = request.form['text'].lower()
-    body = api.park_total(text)
+    text = request.form['text'].upper()
+    state = get_state_name(text)
+    body = park_total(text)
+    parks = state_search(text)
+    for park in parks:
+        '''flash('{}\r'.format(park['name']))
+        flash('{}\r'.format(park['code']))
+        flash('{}\r'.format(park['desc']))
+        acts = park['act']
+        for act in acts:
+            flash(act['name'])'''
+        flash('<div class=\"container\"><p class=\"name\"> {} </p></div>)'.format(park))
+        flash('\r\n')
     
-    return render_template('search.html', term=text.upper(), body=body)
+    #return '<html><head>  <title> Easy Parks </title></head><body><h1> {} </h1><h3> {} </h3></body></html>'.format(park_total(text), get_flashed_messages()) 
+    return render_template('search.html', parks=parks)
 
+@app.route('/park', methods=['GET', 'POST'])
+def park_page():
+    park_code = request.query_string.decode()
+    park = park_search(park_code)
+    return render_template('park.html', park=park)
 
+app.config.update(SECRET_KEY=os.urandom(24))
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, threaded=True)
